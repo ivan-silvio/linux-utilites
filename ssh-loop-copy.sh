bd68c56db598d0db42d2ae7ffa7ddf9865775851
ivan silvio <ivan.sl@hotmail.com>
2020-10-04 18:48:57 -0300

    Add files via upload

--- ssh-loop-copy.sh
+++ ssh-loop-copy.sh
@@ -6,21 +6,29 @@ read -p "senha para o copy id: " senha
 #
 #
 #
-read -p "caminho do arquivo com a lista de ips dos servidores: " servers
+read -p "o nome arquivo com a lista de ips dos servidores: " servers
 #
 #
 #
+read -p "tempo em segundos de timeout para cada host(dependendo da rede pode ser necessario aumentar): " time
+##
+#Requisitos#########################################################################
+#
+#
+#
+diretorio=`pwd`
 yum install -y sshpass
 zypper install -y sshpass
 sudo apt-get install -y sshpass
+#####################################################################################
 #
 #
-for servers in `cat $servers` 
+for servers in `cat $diretorio/$servers` 
 do
 
 echo $servers 
 
-timeout 0.5 sshpass -p "$senha" ssh-copy-id -f -i ~/.ssh/id_rsa.pub $usuario@$servers
+timeout $time sshpass -p "$senha" ssh-copy-id -f -i ~/.ssh/id_rsa.pub $usuario@$servers
 
 
 done
